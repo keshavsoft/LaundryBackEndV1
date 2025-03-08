bf23fc9da26c9add10d682adbb1c165473d822ca
keshavsoft <nknnkumar@yahoo.com>
2025-03-08 15:18:02 +0530

    show stopped in branch reports qrcodes

--- Custom/Cleaning/Branch/Reports/AllQrcodes/kLowDb/CommonFuncs/QrCodes.js
+++ Custom/Cleaning/Branch/Reports/AllQrcodes/kLowDb/CommonFuncs/QrCodes.js
@@ -1,9 +1,10 @@
-import { StartFunc as PullData } from "../../../../../../../binV4/QrCodes/Show/kLowDb/PullData/returnAsArray.js";
+import { StartFunc as StartFuncPullData } from "../../../../../../../binV4/QrCodes/CommonPull/kLowDb/PullData/returnAsArray.js";
+// import { StartFunc as PullData } from "../../../../../../../binV4/QrCodes/Show/kLowDb/PullData/returnAsArray.js";
 
 let StartFunc = ({ inBranch }) => {
     let LocalBranchName = inBranch;
 
-    let LocalQrCodeOriginalData = PullData();
+    let LocalQrCodeOriginalData = StartFuncPullData();
 
     let LocalFilteredData = LocalQrCodeOriginalData.filter(e => {
         return e.BookingData.OrderData.BranchName === LocalBranchName;
