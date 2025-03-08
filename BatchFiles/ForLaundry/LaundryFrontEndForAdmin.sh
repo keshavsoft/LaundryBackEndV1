4d162cd3b1edf3adffa9b3eb1b1ac89853195e05
keshavsoft <nknnkumar@yahoo.com>
2025-03-08 15:31:33 +0530

    LaundryFrontEndForAdmin.sh good

--- BatchFiles/ForLaundry/LaundryFrontEndForAdmin.sh
+++ BatchFiles/ForLaundry/LaundryFrontEndForAdmin.sh
@@ -1,10 +1,10 @@
+mkdir public/Laundry/Admin
 
 cd ../LaundryFrontEndForAdmin
+git pull
 
 npm run Menu
-
 npm run AdminAllDays-CommonConfig-Menu
-
 npm run AdminAllDays-CommonConfig-QrCode-AsArray
 npm run AdminAllDays-CommonConfig-QrCode-BranchWise
 npm run AdminAllDays-CommonConfig-QrCode-BranchFilter
@@ -12,6 +12,8 @@ npm run AdminAllDays-CommonConfig-QrCodeAtStage-AsArray
 npm run AdminAllDays-CommonConfig-QrCodeAtStage-BranchFilter
 npm run AdminAllDays-CommonConfig-QrCodeAtStage-BranchWise
 npm run Admin-Masters
+npm run Admin-Customers
+npm run Admin-Detailed-Masters
 npm run Accounts
 npm run Dcs
 npm run Orders
