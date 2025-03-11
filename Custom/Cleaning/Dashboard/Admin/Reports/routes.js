35b4eb597dc40fa79bded5ced823a2b7d9384266
pushpadamalanka82 <y*********@gmail.com>
2025-03-11 19:49:13 +0500

    new endpoint

--- Custom/Cleaning/Dashboard/Admin/Reports/routes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/routes.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 import { router as Pos } from './Pos/routes.js';
 import { router as Accounts } from './Accounts/routes.js';
+import { router as Common } from './Common/routes.js';
 
 router.use('/Pos', Pos);
 router.use('/Accounts', Accounts);
+router.use('/Common', Common);
 
 export { router };
