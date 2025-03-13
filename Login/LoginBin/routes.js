544f1ef8766ba64d6b112c3a098a8f4789d84518
Srivasu215 <sri.vasu215@gmail.com>
2025-03-13 13:21:45 +0530

    Branc for logIn

--- Login/LoginBin/routes.js
+++ Login/LoginBin/routes.js
@@ -7,9 +7,11 @@ var router = express.Router();
 import { router as routerFromCreateToken } from './CreateToken/routes.js';
 import { router as routerFromUsers } from "./Users/routes.js";
 import { router as routerFromValidateToken } from "./ValidateToken/routes.js";
+import { router as Branch } from "./Branch/routes.js";
 
 router.use('/CreateToken', routerFromCreateToken);
 router.use('/Users', routerFromUsers);
 router.use('/ValidateToken', routerFromValidateToken);
+router.use('/Branch', Branch);
 
 export { router };
