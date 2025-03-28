137e80ae74119f699f6b68b51373b9e7aba28d56
pushpadamalanka82 <y*********@gmail.com>
2025-03-28 17:33:01 +0530

    some changes

--- Custom/Cleaning/Factory/V1/Process/Washing/Scan/Voucher/dals/getFuncs/EntryFile.js
+++ Custom/Cleaning/Factory/V1/Process/Washing/Scan/Voucher/dals/getFuncs/EntryFile.js
@@ -1,6 +1,7 @@
 import { StartFunc as GetVoucher } from '../../kLowDb/ReadFileList/GetVoucher.js';
 import { StartFunc as GetVoucherQrStatus } from '../../kLowDb/ReadFileList/GetVoucherQrStatus.js';
 import { StartFunc as GetRowData } from '../../kLowDb/ReadFileList/GetRowData.js';
+import { StartFunc as Filter } from '../../kLowDb/ReadFileList/Filter.js';
 
 let GetFunc = ({ inFactory }) => {
     return GetVoucher({ inFactory });
@@ -14,6 +15,10 @@ let GetRowDataFunc = ({ inId }) => {
     return GetRowData({ inId });
 };
 
+let GetFilterFunc = ({ inFactory, fromDate, toDate }) => {
+    return Filter({ inFactory, fromDate, toDate });
+};
+
 export {
-    GetFunc, GetQrStatusFunc, GetRowDataFunc
+    GetFunc, GetQrStatusFunc, GetRowDataFunc, GetFilterFunc
 };
