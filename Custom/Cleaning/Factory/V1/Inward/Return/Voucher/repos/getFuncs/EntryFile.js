202bd450d4880881011993302498cafd103dcf74
sharath0036 <ksharathbabu2@gmail.com>
2025-04-04 18:39:42 +0530

    end point for Dc Details

--- Custom/Cleaning/Factory/V1/Inward/Return/Voucher/repos/getFuncs/EntryFile.js
+++ Custom/Cleaning/Factory/V1/Inward/Return/Voucher/repos/getFuncs/EntryFile.js
@@ -1,11 +1,19 @@
 import {
-    GetFunc as GetFuncDal
+    GetFunc as GetFuncDal,
+	GetidFunc as GetidFuncDal
 } from '../../dals/getFuncs/EntryFile.js';
 
 let GetFunc = ({ inFactory }) => {
     return GetFuncDal({ inFactory });
 };
 
+let GetidFunc = async ({id}) => {
+	let LocalFromDal = await GetidFuncDal({id});
+
+	return LocalFromDal;
+};
+
 export {
-    GetFunc
+    GetFunc,
+	GetidFunc
 };
