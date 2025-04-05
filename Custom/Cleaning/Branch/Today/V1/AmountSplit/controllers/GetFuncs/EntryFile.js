29c99a03c0ad7ef9184dca88a5a03592dd39a1f2
sharath0036 <ksharathbabu2@gmail.com>
2025-04-05 19:44:57 +0530

    end points updated

--- Custom/Cleaning/Branch/Today/V1/AmountSplit/controllers/GetFuncs/EntryFile.js
+++ Custom/Cleaning/Branch/Today/V1/AmountSplit/controllers/GetFuncs/EntryFile.js
@@ -1,5 +1,8 @@
 import {
-    GetFuncs as GetFuncsRepo
+    GetFuncs as GetFuncsRepo,
+    GetCashFuncs as GetCashFuncsRepo,
+    GetCardFuncs as GetCardFuncsRepo,
+    GetUpiFuncs as GetUpiFuncsRepo
 } from '../../repos/GetFuncs/EntryFile.js';
 
 let GetFuncs = (req, res) => {
@@ -16,6 +19,48 @@ let GetFuncs = (req, res) => {
     res.status(200).json(LocalFromRepo);
 };
 
+let GetCashFuncs = (req, res) => {
+    let LocalParams = req.params;
+    let LocalBranch = LocalParams.inBranch
+
+    let LocalFromRepo = GetCashFuncsRepo({ inBranch: LocalBranch });
+
+    if (LocalFromRepo.KTF === false) {
+        res.status(500).send(LocalFromRepo.KReason);
+        return;
+    };
+
+    res.status(200).json(LocalFromRepo);
+};
+
+let GetCardFuncs = (req, res) => {
+    let LocalParams = req.params;
+    let LocalBranch = LocalParams.inBranch
+
+    let LocalFromRepo = GetCardFuncsRepo({ inBranch: LocalBranch });
+
+    if (LocalFromRepo.KTF === false) {
+        res.status(500).send(LocalFromRepo.KReason);
+        return;
+    };
+
+    res.status(200).json(LocalFromRepo);
+};
+
+let GetUpiFuncs = (req, res) => {
+    let LocalParams = req.params;
+    let LocalBranch = LocalParams.inBranch
+
+    let LocalFromRepo = GetUpiFuncsRepo({ inBranch: LocalBranch });
+
+    if (LocalFromRepo.KTF === false) {
+        res.status(500).send(LocalFromRepo.KReason);
+        return;
+    };
+
+    res.status(200).json(LocalFromRepo);
+};
+
 export {
-    GetFuncs
+    GetFuncs, GetCardFuncs, GetUpiFuncs, GetCashFuncs
 };
