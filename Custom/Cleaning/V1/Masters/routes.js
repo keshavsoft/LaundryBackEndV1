2c9c45499ea095a8b8e18d2ab5f6de9f996efbd1
keshavsoft <nknnkumar@yahoo.com>
2025-04-12 16:44:14 +0530

    MastersItemTypes in v1 show all good

--- Custom/Cleaning/V1/Masters/routes.js
+++ Custom/Cleaning/V1/Masters/routes.js
@@ -5,9 +5,11 @@ var router = express.Router();
 import { router as routerFromCustomers } from './Customers/routes.js';
 import { router as routerFromAddOns } from './AddOns/routes.js';
 import { router as routerFromItemNames } from './ItemNames/routes.js';
+import { router as routerFromMastersItemTypes } from './MastersItemTypes/routes.js';
 
 router.use('/Customers', routerFromCustomers);
 router.use('/AddOns', routerFromAddOns);
 router.use('/ItemNames', routerFromItemNames);
+router.use('/MastersItemTypes', routerFromMastersItemTypes);
 
 export { router };
