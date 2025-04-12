9b53a0839d3d3d362179a0608d0e17d254c7cb3e
keshavsoft <nknnkumar@yahoo.com>
2025-04-12 15:21:46 +0530

    Masters.bat good

---
+++ BatchFiles/ForLaundry/V1/Masters.bat
@@ -0,0 +1,11 @@
+@echo off
+cd ..\FrontEndCustomers
+git pull
+
+rmdir /s /q .\publicDir\*
+
+call gulp build:publicDir
+
+xcopy .\publicDir ..\LaundryBackEndV1\public\Laundry\V1\Masters /h /i /c /k /e /r /y
+
+cd ..\LaundryBackEndV1
